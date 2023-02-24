4cb5f77f801e550f1cac2f4adf7eb628368ffd18
Villager <deiondrae@gmail.com>
2023-02-24 16:33:45 -0500

    finished Navigation component and Contact component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import About from './components/about';
 import Projects from './components/projects';
 import Skills from './components/skills';
 import Contact from './components/contact';
+import Navbar from './components/nav';
 
 function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar></Navbar>
       <About></About>
       <Projects></Projects>
       <Skills></Skills>
