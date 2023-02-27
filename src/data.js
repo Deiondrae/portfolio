5ef04447a4d16b8150122d7ee78a585b7be2f967
Villager <deiondrae@gmail.com>
2023-02-27 17:02:03 -0500

    fixed casing

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@
 export const projects = [
       {
       title: "E-Commerce back-end",
-      subtitle: "Javascript, Express.js, MySQL2, Sequelize",
+      subtitle: "JavaScript, Express.js, MySQL2, Sequelize",
       description:
         "This application is a backend for getting, creating, updating, and deleting items for an e-commerce website (i.e., products, categories, product tags, etc.). The application uses sequelize to handle the mapping and associations with the MySQL2 database.",
       image: "./e-commerce.png",
@@ -10,7 +10,7 @@ export const projects = [
     },
     {
       title: "Social Network API",
-      subtitle: "Javascript, Express.js, MongoDB, Mongoose",
+      subtitle: "JavaScript, Express.js, MongoDB, Mongoose",
       description:
         "An api for a social media network web application where users can share their thoughts, react to friends' thoughts and create a friendslist. Express.js was used for routing, a mongoDB database and the mongoose ODM",
       image: "./social-network.png",
@@ -18,7 +18,7 @@ export const projects = [
     },
     {
       title: "Employee-Tracker",
-      subtitle: "Javascript, inquirer, MySQL2",
+      subtitle: "JavaScript, Inquirer, MySQL2",
       description:
         "This employee tracker is a content management system used to manage a company's employeee database, using Node.js, Inquirer, and MySQL",
       image: "./employee-tracker.png",
@@ -26,7 +26,7 @@ export const projects = [
     },
     {
       title: "Password generator",
-      subtitle: "HTML, CSS, Javascript",
+      subtitle: "HTML, CSS, JavaScript",
       description:
         "A javascript based password generation tool that prompts for password length, as well as the use of numbers, special characters, lowercase characters and uppercase characters",
       image: "./Password.gif",
@@ -34,7 +34,7 @@ export const projects = [
     },
     {
       title: "Work Day Scheduler",
-      subtitle: "HTML, CSS, Javascript",
+      subtitle: "HTML, CSS, JavaScript",
       description:
         "A simple calendar application that allows a user to save events for each hour of the day",
       image: "./workday.png",
@@ -42,7 +42,7 @@ export const projects = [
     },
     {
       title: "Taskmaster-pro",
-      subtitle: "HTML, CSS, Javascript",
+      subtitle: "HTML, CSS, JavaScript",
       description:
         "Task management app with persistent task data and due date warnings",
       image: "./taskmaster.gif",
